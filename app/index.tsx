71a4bdbbb8a256f1f4e528eb7986dc379b572188
unknown <indaygclifft4@gmail.com>
2026-04-13 23:41:30 +0800

    prefinal exam

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function prefiExam() {
+export default function index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
       <Text style={styles.content}>G-ClIFFT ROSALES INDAY. BSIT-2A</Text>
     </View>  
   );
@@ -11,13 +10,11 @@ export default function prefiExam() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "black",
     fontSize: 50,
-    flexDirection: "coloumn",
-    gap: 10,
   },
   content: {
         fontSize: 30,
